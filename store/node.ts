d49a25973d4503ff6f7adc10e3a005190180aed9
zhaoqiaochu <c9cu@qq.com>
2023-10-03 17:54:48 +0800

    add method to update gist

--- store/node.ts
+++ store/node.ts
@@ -3,17 +3,25 @@ import { StorageValue, persist } from 'zustand/middleware'
 
 export interface NodesStore {
   nodesInput: string
+  gistId: string
+  githubToken: string
   nodeMap: Record<string, any>
   getIp: (ip: string) => any
   setIp: (ip: string, value: any) => void
   setNodesInput: (nodesInput: string) => void
+  setGistId: (gistId: string) => void
+  setGithubToken: (githubToken: string) => void
 }
 
 export const useNodesStore = create<NodesStore>()(
   persist(
     (set, get) => ({
       nodesInput: '',
+      gistId: '',
+      githubToken: '',
       nodeMap: {},
+      setGistId: (gistId: string) => set({ gistId }),
+      setGithubToken: (githubToken: string) => set({ githubToken }),
       getIp: (ip: string) => get().nodeMap[ip],
       setIp: (ip: string, value: any) =>
         set({ nodeMap: { ...get().nodeMap, [ip]: value } }),
