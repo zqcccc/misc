16dc6c8b3f024da64fd7fb84519d126a9b76cadf
zhaoqiaochu <c9cu@qq.com>
2023-10-10 00:08:35 +0800

    add drag in vmess

--- app/vmess/page.tsx
+++ app/vmess/page.tsx
@@ -1,13 +1,16 @@
 'use client'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 import { useNodesStore } from '@/store/node'
-import { useSetState } from 'ahooks'
+import { useMemoizedFn, useSetState } from 'ahooks'
 import { useEffect, useMemo, useRef } from 'react'
 import { Base64 } from 'js-base64'
 import { copy } from '../post/[...id]/helpers'
 import { getClipboardText } from './utils'
 import message from 'antd/es/message'
 import 'antd/es/message/style'
+import VmessItem from './vmessItem'
 
 const emojiMap: Record<string, string> = {
   CN: '🇨🇳',
@@ -152,122 +155,87 @@ export default function NodeConfig() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.nodeList])
 
-  return (
-    <main className='p-3'>
-      <h2>Node Config input</h2>
-      <label>
-        <input
-          type='checkbox'
-          checked={state.willGetLoc}
-          onChange={(e) => {
-            setState({ willGetLoc: e.target.checked })
-          }}
-        />
-        是否要获取ip地区信息
-      </label>
-      <textarea
-        rows={10}
-        className='w-full mt-2 p-2'
-        placeholder='ss/ssr/vmess链接，多个链接每行一个'
-        value={NodesStore.nodesInput}
-        onChange={(e) => NodesStore.setNodesInput(e.target.value)}
-      ></textarea>
+  const moveItem = useMemoizedFn((index: number, toIndex: number) => {
+    const nodeList = state.nodeList.concat()
+    const item = nodeList[index]
+    nodeList.splice(index, 1)
+    nodeList.splice(toIndex, 0, item)
+    setState({ nodeList })
+  })
 
-      <div>
-        <button
-          className='py-1 px-2'
-          onClick={() => {
-            getClipboardText().then((text) => {
-              console.log('text: ', text)
-              if (text) {
-                NodesStore.setNodesInput(`${text}\n${NodesStore.nodesInput}`)
-                message.success('已添加到列表头部')
-              }
-            })
-          }}
-        >
-          append to head of list(clipboard)
-        </button>
-        <button
-          className='py-1 px-2 ml-1'
-          onClick={() => {
-            getClipboardText().then((text) => {
-              if (text) {
-                NodesStore.setNodesInput(`${NodesStore.nodesInput}\n${text}`)
-                message.success('已添加到列表尾部')
-              }
-            })
-          }}
-        >
-          append to tail of list(clipboard)
-        </button>
-      </div>
-      <input
-        type='text'
-        placeholder='统一改名'
-        value={state.unifyName}
-        onChange={(e) => setState({ unifyName: e.target.value })}
-      />
-      <div className='mt-3'>
-        <h2>node list</h2>
-        <div className='flex w-full flex-wrap'>
-          {state.nodeList.map(([_, item], index) => {
-            const keys = Object.keys(item)
-            return (
-              <div className='m-3' key={index}>
-                <h3>
-                  No.{index} {item?.ps && `name:${item?.ps}`}
-                </h3>
-                {keys.map((key, keyIndex) => {
-                  return (
-                    <div key={keyIndex}>
-                      <input
-                        className='w-12'
-                        value={key}
-                        onChange={(e) => {
-                          const nodeList = state.nodeList.concat()
-                          const newItem = nodeList[index].concat()
-                          const v = item[key]
-                          delete item[key]
-                          newItem[1] = Object.assign({}, item, {
-                            [e.target.value]: v,
-                          })
-                          nodeList[index] = newItem
-                          setState({ nodeList })
-                        }}
-                      />
-                      <span> : </span>
-                      <input
-                        value={item[key]}
-                        onChange={(e) => {
-                          const nodeList = state.nodeList.concat()
-                          const newItem = nodeList[index].concat()
-                          newItem[1] = Object.assign({}, item, {
-                            [key]: e.target.value,
-                          })
-                          nodeList[index] = newItem
-                          setState({ nodeList })
-                        }}
-                      ></input>
-                      <button
-                        className='px-1 ml-1'
-                        onClick={() => {
-                          const nodeList = state.nodeList.concat()
-                          const newItem = nodeList[index].concat()
-                          newItem[1] = Object.assign({}, item)
-                          nodeList[index] = newItem
-                          delete nodeList[index][1][key]
-                          setState({ nodeList })
-                        }}
-                      >
-                        x
-                      </button>
-                    </div>
-                  )
-                })}
-                <button
-                  className='px-3 mt-1'
-                  onClick={() => {
+  const moveToLast = useMemoizedFn((index: number) => {
+    moveItem(index, state.nodeList.length - 1)
+  })
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <main className='p-3'>
+        <h2>Node Config input</h2>
+        <label>
+          <input
+            type='checkbox'
+            checked={state.willGetLoc}
+            onChange={(e) => {
+              setState({ willGetLoc: e.target.checked })
+            }}
+          />
+          是否要获取ip地区信息
+        </label>
+        <textarea
+          rows={10}
+          className='w-full mt-2 p-2'
+          placeholder='ss/ssr/vmess链接，多个链接每行一个'
+          value={NodesStore.nodesInput}
+          onChange={(e) => NodesStore.setNodesInput(e.target.value)}
+        ></textarea>
+
+        <div>
+          <button
+            className='py-1 px-2'
+            onClick={() => {
+              getClipboardText().then((text) => {
+                console.log('text: ', text)
+                if (text) {
+                  NodesStore.setNodesInput(`${text}\n${NodesStore.nodesInput}`)
+                  message.success('已添加到列表头部')
+                }
+              })
+            }}
+          >
+            append to head of list(clipboard)
+          </button>
+          <button
+            className='py-1 px-2 ml-1'
+            onClick={() => {
+              getClipboardText().then((text) => {
+                if (text) {
+                  NodesStore.setNodesInput(`${NodesStore.nodesInput}\n${text}`)
+                  message.success('已添加到列表尾部')
+                }
+              })
+            }}
+          >
+            append to tail of list(clipboard)
+          </button>
+        </div>
+        <input
+          type='text'
+          placeholder='统一改名'
+          value={state.unifyName}
+          onChange={(e) => setState({ unifyName: e.target.value })}
+        />
+        <div className='mt-3'>
+          <h2>node list</h2>
+          <div className='flex w-full flex-wrap'>
+            {state.nodeList.map(([_, item], index) => {
+              return (
+                <VmessItem
+                  key={index}
+                  item={item}
+                  index={index}
+                  moveItem={moveItem}
+                  moveToLast={moveToLast}
+                  onAddField={() => {
                     const nodeList = state.nodeList.concat()
                     const newItem = nodeList[index].concat()
                     newItem[1] = Object.assign({}, item, {
@@ -276,162 +244,179 @@ export default function NodeConfig() {
                     nodeList[index] = newItem
                     setState({ nodeList })
                   }}
-                >
-                  +
-                </button>
-                <br />
-                <button
-                  className='px-2 mt-1'
-                  onClick={() => {
-                    const nodeList = state.nodeList.concat()
-                    const newItem = nodeList[index].concat()
-                    nodeList.splice(index + 1, 0, newItem)
-                    setState({ nodeList })
-                  }}
-                >
-                  duplicate this one
-                </button>
-                <br />
-                <button
-                  className='mt-1 px-2'
-                  onClick={() => {
+                  onDelete={() => {
                     if (!window.confirm('delete this one?')) return
                     const nodeList = state.nodeList.concat()
                     nodeList.splice(index, 1)
                     setState({ nodeList })
                   }}
-                >
-                  delete this one
-                </button>
-              </div>
-            )
-          })}
-        </div>
-      </div>
-      <div className='mt-3'>
-        <h2>output configs</h2>
-        <div className='mb-1'>
-          <button
-            className='p-2'
-            onClick={() => {
-              NodesStore.setNodesInput(output)
-              message.info('已设置到 Node Config input')
-            }}
-          >
-            save(set to input)
-          </button>
-          <button
-            className='p-2 ml-2'
-            onClick={() => {
-              copy(output)
-              message.info('已复制')
-            }}
-          >
-            copy
-          </button>
-        </div>
-        <textarea
-          readOnly
-          value={output}
-          rows={10}
-          className='w-full'
-        ></textarea>
-      </div>
-      <div className='mt-3'>
-        <h2>output v2ray configs</h2>
-        <div className='mb-1'>
-          <button
-            className='p-2'
-            onClick={() => {
-              copy(btoa(output))
-              message.info('base64 已复制')
-            }}
-          >
-            copy
-          </button>
-        </div>
-        <textarea
-          readOnly
-          value={btoa(output)}
-          rows={10}
-          className='w-full'
-        ></textarea>
-      </div>
-      <div className='mt-3 pb-10'>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            setState({ isSubmitting: true })
-            const gistId = (
-              document.getElementById('gist-id') as HTMLInputElement
-            ).value
-            const githubToken = (
-              document.getElementById('github-token') as HTMLInputElement
-            ).value
-            fetch(`https://api.github.com/gists/${gistId}`, {
-              method: 'PATCH',
-              headers: {
-                Authorization: `token ${githubToken}`,
-              },
-              body: JSON.stringify({
-                files: {
-                  o: {
-                    content: output,
-                  },
-                  b: {
-                    content: btoa(output),
-                  },
-                },
-              }),
-            })
-              .then((res) => {
-                alert('更新成功')
-                res.json().then((data) => {
-                  console.log(data)
-                })
-              })
-              .catch((err) => {
-                alert(`更新失败: ${err.toString()}`)
-              })
-              .finally(() => {
-                setState({ isSubmitting: false })
-              })
-          }}
-        >
-          <div>
-            <label htmlFor='gist-id'>github gist id: </label>
-            <input
-              id='gist-id'
-              type='text'
-              className='w-72'
-              required
-              value={NodesStore.gistId}
-              onChange={(e) => NodesStore.setGistId(e.target.value)}
-            />
+                  onDeleteField={(key) => () => {
+                    const nodeList = state.nodeList.concat()
+                    const newItem = nodeList[index].concat()
+                    newItem[1] = Object.assign({}, item)
+                    nodeList[index] = newItem
+                    delete nodeList[index][1][key]
+                    setState({ nodeList })
+                  }}
+                  onDuplicate={() => {
+                    const nodeList = state.nodeList.concat()
+                    const newItem = nodeList[index].concat()
+                    nodeList.splice(index + 1, 0, newItem)
+                    setState({ nodeList })
+                  }}
+                  onKeyChange={(key) => (e) => {
+                    const nodeList = state.nodeList.concat()
+                    const newItem = nodeList[index].concat()
+                    const v = item[key]
+                    delete item[key]
+                    newItem[1] = Object.assign({}, item, {
+                      [e.target.value]: v,
+                    })
+                    nodeList[index] = newItem
+                    setState({ nodeList })
+                  }}
+                  onValueChange={(key) => (e) => {
+                    const nodeList = state.nodeList.concat()
+                    const newItem = nodeList[index].concat()
+                    newItem[1] = Object.assign({}, item, {
+                      [key]: e.target.value,
+                    })
+                    nodeList[index] = newItem
+                    setState({ nodeList })
+                  }}
+                  onCopy={() => {
+                    copy(output.split('\n').filter(Boolean)[index])
+                  }}
+                />
+              )
+            })}
           </div>
-          <div>
-            <label htmlFor='github-token'>github token: </label>
-            <input
-              id='github-token'
-              type='text'
-              className='w-96'
-              required
-              value={NodesStore.githubToken}
-              onChange={(e) => NodesStore.setGithubToken(e.target.value)}
-            />
+        </div>
+        <div className='mt-3'>
+          <h2>output configs</h2>
+          <div className='mb-1'>
+            <button
+              className='p-2'
+              onClick={() => {
+                NodesStore.setNodesInput(output)
+                message.info('已设置到 Node Config input')
+              }}
+            >
+              save(set to input)
+            </button>
+            <button
+              className='p-2 ml-2'
+              onClick={() => {
+                copy(output)
+                message.info('已复制')
+              }}
+            >
+              copy
+            </button>
           </div>
-          <button
-            type='submit'
-            className='p-2 mt-2'
-            disabled={state.isSubmitting}
+          <textarea
+            readOnly
+            value={output}
+            rows={10}
+            className='w-full'
+          ></textarea>
+        </div>
+        <div className='mt-3'>
+          <h2>output v2ray configs</h2>
+          <div className='mb-1'>
+            <button
+              className='p-2'
+              onClick={() => {
+                copy(btoa(output))
+                message.info('base64 已复制')
+              }}
+            >
+              copy
+            </button>
+          </div>
+          <textarea
+            readOnly
+            value={btoa(output)}
+            rows={10}
+            className='w-full'
+          ></textarea>
+        </div>
+        <div className='mt-3 pb-10'>
+          <form
             onSubmit={(e) => {
-              console.log(e)
+              e.preventDefault()
+              setState({ isSubmitting: true })
+              const gistId = (
+                document.getElementById('gist-id') as HTMLInputElement
+              ).value
+              const githubToken = (
+                document.getElementById('github-token') as HTMLInputElement
+              ).value
+              fetch(`https://api.github.com/gists/${gistId}`, {
+                method: 'PATCH',
+                headers: {
+                  Authorization: `token ${githubToken}`,
+                },
+                body: JSON.stringify({
+                  files: {
+                    o: {
+                      content: output,
+                    },
+                    b: {
+                      content: btoa(output),
+                    },
+                  },
+                }),
+              })
+                .then((res) => {
+                  message.success('更新成功')
+                  res.json().then((data) => {
+                    console.log(data)
+                  })
+                })
+                .catch((err) => {
+                  alert(`更新失败: ${err.toString()}`)
+                })
+                .finally(() => {
+                  setState({ isSubmitting: false })
+                })
             }}
           >
-            save to github gist
-          </button>
-        </form>
-      </div>
-    </main>
+            <div>
+              <label htmlFor='gist-id'>github gist id: </label>
+              <input
+                id='gist-id'
+                type='text'
+                className='w-72'
+                required
+                value={NodesStore.gistId}
+                onChange={(e) => NodesStore.setGistId(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor='github-token'>github token: </label>
+              <input
+                id='github-token'
+                type='text'
+                className='w-96'
+                required
+                value={NodesStore.githubToken}
+                onChange={(e) => NodesStore.setGithubToken(e.target.value)}
+              />
+            </div>
+            <button
+              type='submit'
+              className='p-2 mt-2'
+              disabled={state.isSubmitting}
+              onSubmit={(e) => {
+                console.log(e)
+              }}
+            >
+              save to github gist
+            </button>
+          </form>
+        </div>
+      </main>
+    </DndProvider>
   )
 }
---
+++ app/vmess/vmessItem.tsx
@@ -0,0 +1,160 @@
+import { ChangeEvent, useRef } from 'react'
+import type { Identifier, XYCoord } from 'dnd-core'
+import { useDrag, useDrop } from 'react-dnd'
+
+type VmessItemProps = {
+  item: any
+  index: number
+  onKeyChange: (key: string) => (e: ChangeEvent<HTMLInputElement>) => void
+  onValueChange: (key: string) => (e: ChangeEvent<HTMLInputElement>) => void
+  onCopy: () => void
+  onAddField: () => void
+  onDeleteField: (key: string) => () => void
+  onDuplicate: () => void
+  onDelete: () => void
+  moveItem: (dragIndex: number, hoverIndex: number) => void
+  moveToLast: (index: number) => void
+}
+
+interface DragItem {
+  index: number
+  id: string
+  type: string
+}
+
+const VmessItem = (props: VmessItemProps) => {
+  const {
+    item,
+    index,
+    onKeyChange,
+    onValueChange,
+    onCopy,
+    onAddField,
+    onDeleteField,
+    onDuplicate,
+    onDelete,
+    moveItem,
+    moveToLast,
+  } = props
+  const ref = useRef<HTMLDivElement>(null)
+  const keys = Object.keys(item)
+  const id = `${Object.values(item).join(',')}_${index}`
+  const [{ handlerId }, drop] = useDrop<
+    DragItem,
+    void,
+    { handlerId: Identifier | null }
+  >({
+    accept: 'Vmess',
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      }
+    },
+    hover(item: DragItem, monitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = index
+
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return
+      }
+
+      // Determine rectangle on screen
+      // const hoverBoundingRect = ref.current?.getBoundingClientRect()
+
+      // Get vertical middle
+      // const hoverMiddleY =
+      // (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+
+      // Determine mouse position
+      // const clientOffset = monitor.getClientOffset()
+
+      // Get pixels to the top
+      // const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top
+
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+
+      // Dragging downwards
+      // if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+      //   return
+      // }
+
+      // // Dragging upwards
+      // if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+      //   return
+      // }
+
+      // Time to actually perform the action
+      moveItem(dragIndex, hoverIndex)
+
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex
+    },
+  })
+
+  const [{ isDragging }, drag] = useDrag({
+    type: 'Vmess',
+    item: () => {
+      return { id, index }
+    },
+    collect: (monitor: any) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+  drag(drop(ref))
+  return (
+    <div
+      className={`m-3 ${isDragging ? 'border' : ''}`}
+      ref={ref}
+      data-handler-id={handlerId}
+    >
+      <h3>
+        No.{index} {item?.ps && `name:${item?.ps}`}
+      </h3>
+      {keys.map((key, keyIndex) => {
+        return (
+          <div key={keyIndex}>
+            <input className='w-12' value={key} onChange={onKeyChange(key)} />
+            <span> : </span>
+            <input value={item[key]} onChange={onValueChange(key)}></input>
+            <button className='px-1 ml-1' onClick={onDeleteField(key)}>
+              x
+            </button>
+          </div>
+        )
+      })}
+      <button className='px-3 mt-1' onClick={onAddField}>
+        +
+      </button>
+      <button className='px-3 mt-1 ml-1' onClick={onCopy}>
+        copy
+      </button>
+      <br />
+      <button className='px-2 mt-1' onClick={onDuplicate}>
+        duplicate this one
+      </button>
+      <br />
+      <button className='mt-1 px-2' onClick={onDelete}>
+        delete this one
+      </button>
+      <br />
+      <button className='mt-1 px-2' onClick={() => moveItem(index, 0)}>
+        move to fist one
+      </button>
+      <br />
+      <button className='mt-1 px-2' onClick={() => moveToLast(index)}>
+        move to last one
+      </button>
+    </div>
+  )
+}
+
+export default VmessItem
