36cf13143ca122fe74c74fc182fd519a9d059d4b
zqcccc <19168323+zqcccc@users.noreply.github.com>
2023-10-22 17:07:54 +0800

    short

--- app/api/shorter/route.ts
+++ app/api/shorter/route.ts
@@ -25,3 +25,20 @@ export const POST = async (request: Request) => {
 
   return new Response(`${process.env.SHORTENER_PREFIX}/url/${key}`)
 }
+
+export const PATCH = async (request: Request) => {
+  const apiUrl = process.env.REMOTE_API
+  const res = await fetch(`${apiUrl}/url`, {
+    method: 'PATCH',
+    body: request.body,
+    headers: {
+      'Content-Type': request.headers.get('Content-Type') || 'application/json',
+    },
+    // @ts-ignore
+    duplex: 'half',
+    cache: 'no-store',
+  })
+  const key = await res.text()
+
+  return new Response(`${process.env.SHORTENER_PREFIX}/url/${key}`)
+}
