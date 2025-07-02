674545118a4339a54a2155062f4acfdfbc4fd93d
zhaoqiaochu <c9cu@qq.com>
2025-07-02 20:07:31 +0800

    feat: Integrate Clarity tracking script into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,13 @@ export default function RootLayout({
   return (
     <html lang='zh-Hans'>
       <body>{children}</body>
+      <Script id='clarity'>
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "s8lmijoo3v");`}
+      </Script>
       <Script
         async
         src='https://www.googletagmanager.com/gtag/js?id=G-4DLMMFXJMP'
